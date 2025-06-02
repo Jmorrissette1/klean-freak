5fdf5460f15747f636b8bb01608394ff3966972c
Jason Morrissette <j.morrissette47@gmail.com>
2025-06-02 12:37:37 -0700

    started building out footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,7 +2,9 @@ import styles from "./Footer.module.css";
 const Footer = () => {
   return (
     <div className={styles.footerBody}>
-      <div>FIND YOUR NEAREST KLEAN FREAK</div>
+      <div className={styles.footerContent}>
+        <h1 className={styles.footerH1}>FIND YOUR NEAREST KLEAN FREAK</h1>
+      </div>
     </div>
   );
 };
