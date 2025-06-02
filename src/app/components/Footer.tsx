114f3a3f2cf5bcf030821dd2acb0d4ebdae76f21
Jason Morrissette <j.morrissette47@gmail.com>
2025-06-02 16:02:50 -0700

    started intregrating rinsed forms

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,7 +3,9 @@ const Footer = () => {
   return (
     <div className={styles.footerBody}>
       <div className={styles.footerContent}>
-        <h1 className={styles.footerH1}>FIND YOUR NEAREST KLEAN FREAK</h1>
+        <h1 className={styles.footerH1}>
+          <em>FIND YOUR NEAREST KLEAN FREAK</em>
+        </h1>
       </div>
     </div>
   );
--- src/app/memberships/page.tsx
+++ src/app/memberships/page.tsx
@@ -1,5 +1,27 @@
 const Memeberships = () => {
-  return <div>Membership Page</div>;
+  return (
+    <div>
+      Membership Page
+      <section className="pageContent">
+        <div className="rinsedFormContainer">
+          <iframe
+            src="https://kleanfreak.app.rinsed.co/ticket_forms/34"
+            loading="lazy"
+            className="rinsed-frame"
+            style={{
+              border: "none",
+              display: "inline-block",
+              width: "90%",
+              height: "800px",
+            }}
+          ></iframe>
+          <div className="membership-link">
+            Dont have a Account? <a href="kleanclub.html">SIGN UP</a>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Memeberships;
