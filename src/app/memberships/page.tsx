957771bfb855c642f10adb5f6584888337f9a9d1
Jason Morrissette <j.morrissette47@gmail.com>
2025-06-03 09:45:38 -0700

    fixed formating issues

--- src/app/memberships/page.tsx
+++ src/app/memberships/page.tsx
@@ -1,25 +1,31 @@
 const Memeberships = () => {
   return (
     <div>
-      Membership Page
       <section className="pageContent">
         <div className="rinsedFormContainer">
           <iframe
             src="https://kleanfreak.app.rinsed.co/ticket_forms/34"
             loading="lazy"
             className="rinsed-frame"
+            width="100%"
+            height="1000px"
+            sandbox="allow-scripts allow-forms"
+            scrolling="no"
             style={{
               border: "none",
               display: "inline-block",
-              width: "90%",
-              height: "800px",
+              overflow: "hidden",
             }}
           ></iframe>
-          <div className="membership-link">
-            Dont have a Account? <a href="kleanclub.html">SIGN UP</a>
-          </div>
+        </div>
+        <div className="membership-link">
+          Dont have a Account? <a href="kleanclub.html">SIGN UP</a>
         </div>
       </section>
+      <script
+        src="https://kleanfreak.app.rinsed.co/frame_parent.js"
+        defer
+      ></script>
     </div>
   );
 };
