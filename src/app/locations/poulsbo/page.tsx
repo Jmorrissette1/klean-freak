43b1a5cdb3229b205499c75c5dd0effd1ac5ac44
Jason Morrissette <j.morrissette47@gmail.com>
2025-07-11 14:16:13 -0700

    finished styling pages, just have home page left

--- src/app/locations/poulsbo/page.tsx
+++ src/app/locations/poulsbo/page.tsx
@@ -18,7 +18,7 @@ const Poulsbo = () => {
             </div>
         
             <div className="locationTextBox">
-                <strong><div className="pageH3">Wash Features</div></strong>
+              <strong><div className="pageH3">Wash Features</div></strong>
                 <ul className="locationFeaturesList"> 
                     <li>Express Exterior Tunnel </li>
                     <li>120 HP of Drying Power</li>
@@ -31,7 +31,8 @@ const Poulsbo = () => {
                     <li>Open 7 days a week (Minus Weather Closures and Holidays)</li>
                 </ul>
             </div>
-            <div className="googleMap"><iframe
+          
+            <div className="google-map"><iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2682.349664021825!2d-122.6638054232711!3d47.75527167769884!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x549023c1a3276761%3A0xfea2be528d64d6cd!2s21205%20Olhava%20Way%20NW%2C%20Poulsbo%2C%20WA%2098370!5e0!3m2!1sen!2sus!4v1708369129564!5m2!1sen!2sus"
                     width="600" height="420" style={{ border: 0 }} allowFullScreen loading="lazy"
                     referrerPolicy="no-referrer-when-downgrade"></iframe>
--- src/app/locations/silverdale/page.tsx
+++ src/app/locations/silverdale/page.tsx
@@ -1,11 +1,12 @@
+import Link from "next/link";
 
 const Silverdale = () => {
-  return  <div className="centeredColumn"><div className="locationbox">
-            <div className="location-text-box">
+  return  <div className="centeredColumn"><div className="locationBox">
+            <div className="locationTextBox">
                 <h1 className="pageH1">Silverdale</h1>
                 <div className="textBlock">
-                    <div className="pageH4">Phone:<a href="tel:+3606163276">360-616-3276 </a> </div>
-                    <div className="pageH4">Email: <a href="mailto:info@kleanfreakcarwash.com">info@kleanfreakcarwash.com</a></div>
+                    <div className="pageH4">Phone:<Link href="tel:+3606163276">360-616-3276 </Link> </div>
+                    <div className="pageH4">Email: <Link href="mailto:info@kleanfreakcarwash.com">info@kleanfreakcarwash.com</Link></div>
                     <div className="pageH4">Address:9550 Ridgetop Blvd,<br /> Silverdale, WA 98383</div>
                 </div>
 
@@ -13,9 +14,9 @@ const Silverdale = () => {
                     <div className="pageH3"><strong>Hours:</strong></div>
                     <div>Open 24 hours!</div>
                 </div>
-                   
-            <div className="location-text-box">
-                <strong><div className="pageH3">Wash Features</div></strong>
+            </div> 
+            <div className="locationTextBox">
+                <div className="pageH3"><strong>Wash Features</strong></div>
                 <ul className="locationFeaturesList">
                     <li>#2 Touchless In Bay Automatics.</li>
                     <li>#4 Self Serve Wash Bays.</li>
@@ -31,11 +32,11 @@ const Silverdale = () => {
             </div>
             <div className="google-map"><iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3800.856234343713!2d-122.68203566346624!3d47.65312142030314!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54903aebc0b25671%3A0xc59c96801613f03a!2s9550%20Ridgetop%20Blvd%20NW%2C%20Silverdale%2C%20WA%2098383!5e0!3m2!1sen!2sus!4v1708386783251!5m2!1sen!2sus"
-                    width="600" height="450" style={{ border: 0 }} allowFullScreen loading="lazy"
+                    width="100%" height="450" style={{ border: 0 }} allowFullScreen loading="lazy"
                     referrerPolicy="no-referrer-when-downgrade"></iframe>
             </div>
             </div>
-      </div>
+      
 };
 
 export default Silverdale;
