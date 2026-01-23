1405104000be12d61aed1697bed70763bcd08c9f
Jason Morrissette <j.morrissette47@gmail.com>
2026-01-23 15:12:20 -0800

    Made a few changes

--- src/app/components/MonthlyWashesBar.tsx
+++ src/app/components/MonthlyWashesBar.tsx
@@ -1,8 +1,8 @@
 const MonthlyWashesBar = () => {
     return (
-        <div className="centeredRow monthlyWashBar">
+        <div className="monthlyWashBar">
             <div className="centeredCol">
-                <h3 className="pageH3 mr-10"><em><strong>UNLIMITED MONTHLY WASHES</strong></em></h3>
+                <h3 className="pageH3"><em><strong>UNLIMITED MONTHLY WASHES</strong></em></h3>
                 <p><em><strong>A Feel Good Klean for less! Starting at $23 a month.</strong></em></p>
             </div>
             <div>
